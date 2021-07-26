98e23df433cbde1aaea13d88688c9bc767fd0479
GitHayato <gabayt0_0@icloud.com>
2021-07-26 15:24:22 +0900

    グローバルスタイルの設定

---
+++ react/react-app/src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        color: "#333333"
+      }
+    }
+  }
+});
+
+export default theme;
