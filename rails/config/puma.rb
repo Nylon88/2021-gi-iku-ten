db3567fba8b7a29dd499aad3071de04ecf19418a
Nylon88 <rezero0502@icloud.com>
2021-07-26 22:18:49 +0900

    コンテナの設計の修正

--- rails/config/puma.rb
+++ rails/config/puma.rb
@@ -2,7 +2,8 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
+# bind        ENV.fetch("IP") { "0.0.0.0" }
+port        ENV.fetch("PORT") { 80 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
