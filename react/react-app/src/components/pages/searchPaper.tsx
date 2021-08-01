9a08f5daad742028a2d496b0130cf9ea8ca271cd
GitHayato <gabayt0_0@icloud.com>
2021-08-01 13:42:43 +0900

    検索ボックスのUI

--- react/react-app/src/components/pages/searchPaper.tsx
+++ react/react-app/src/components/pages/searchPaper.tsx
@@ -1,8 +1,27 @@
+import { Box, Center, Input, InputGroup, InputLeftElement, InputRightElement } from "@chakra-ui/react";
 import { memo } from "react";
 import { VFC } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
 
 export const searchPaper: VFC = memo(() => {
   return (
-    <h1>test</h1>
+    <Center mt={16}>
+      <Box>
+        <InputGroup>
+          <InputLeftElement
+            children={<FaSearch color="#EAEAEA"/>}
+          />
+          <Input
+            w="60%"
+            minW="880px"
+            borderRadius="0"
+            // focusBorderColor="#9AAE09"
+          />
+          <InputRightElement
+            children={<FaTimes color="#EAEAEA" />}
+          />
+        </InputGroup>
+      </Box>
+    </Center>
   )
 })
