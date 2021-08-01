72461e5fa4e252209db11cf1733f4e9351478d9d
GitHayato <gabayt0_0@icloud.com>
2021-08-01 18:24:24 +0900

    ヘッダーの新規登録ボタンの設定とリンク設置

--- react/react-app/src/components/organisms/layout/Header.tsx
+++ react/react-app/src/components/organisms/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { memo, VFC } from "react";
-import { Button, Flex, Image } from "@chakra-ui/react";
-import { useSelector } from "react-redux";
+import { Button, Flex, Image, Spacer } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { push } from "connected-react-router";
 
 import { Selector } from "../../../redux/users/ActionType";
 import { getUserName, getUserState } from "../../../redux/users/selectors";
@@ -10,18 +11,38 @@ export const Header:VFC = memo(() => {
   const userName = getUserName(selector)
   const LoginState = getUserState(selector)
 
+  const dispatch = useDispatch();
+
   return (
-    <Flex mx={32} my={3} align="flex-end" justify="space-between">
+    <Flex mx={32} my={3} align="flex-end">
       <Image
         src={`${process.env.PUBLIC_URL}/PaperPicks.png`}
         alt="PaperPicks Logo"
         htmlWidth="400"
         _hover={{cursor: "pointer"}}
       />
+      <Spacer />
       {LoginState ? (
         <h1>{userName}</h1>
       ) : (
-        <Button mb={3} bg="#406B15" color="white" borderRadius="0" _hover={{opacity: 0.8}}>ログイン</Button>
+        <>
+          <Button
+            mb={3}
+            mx={5}
+            bg="white"
+            borderRadius="0"
+            _hover={{textDecoration: "underline"}}
+            onClick={() => dispatch(push("/sign_up"))}
+          >新規登録</Button>
+          <Button
+            mb={3}
+            bg="#406B15"
+            color="white"
+            borderRadius="0"
+            _hover={{opacity: 0.8}}
+            onClick={() => dispatch(push("/sign_in"))}
+          >ログイン</Button>
+        </>
       )}
     </Flex>
   )
