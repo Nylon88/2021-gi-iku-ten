1ec1a1ca506694b4613bdad984b67995a6b36276
GitHayato <gabayt0_0@icloud.com>
2021-08-01 13:54:29 +0900

    コンポーネントの切り出し

--- react/react-app/src/components/pages/searchPaper.tsx
+++ react/react-app/src/components/pages/searchPaper.tsx
@@ -1,26 +1,14 @@
-import { Box, Center, Input, InputGroup, InputLeftElement, InputRightElement } from "@chakra-ui/react";
+import { Box, Center, Heading, Text } from "@chakra-ui/react";
 import { memo } from "react";
 import { VFC } from "react";
-import { FaSearch, FaTimes } from "react-icons/fa";
+
+import { SearchInput } from "../molecules/searchInput";
 
 export const searchPaper: VFC = memo(() => {
   return (
     <Center mt={16}>
       <Box>
-        <InputGroup>
-          <InputLeftElement
-            children={<FaSearch color="#EAEAEA"/>}
-          />
-          <Input
-            w="60%"
-            minW="880px"
-            borderRadius="0"
-            // focusBorderColor="#9AAE09"
-          />
-          <InputRightElement
-            children={<FaTimes color="#EAEAEA" />}
-          />
-        </InputGroup>
+        <SearchInput />
       </Box>
     </Center>
   )
