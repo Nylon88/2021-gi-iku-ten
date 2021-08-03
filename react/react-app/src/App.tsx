25df83919bceb415c34839b6bef0a6653bc92686
GitHayato <gabayt0_0@icloud.com>
2021-08-03 17:02:35 +0900

    未使用なUserRegistrationの削除

--- react/react-app/src/App.tsx
+++ react/react-app/src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { UserRegistration } from './components/pages/UserRegistration';
 import theme from './theme/theme';
 import { HeaderLayout } from './components/templates/HeaderLayout';
 import { Router } from './components/router/Router';
