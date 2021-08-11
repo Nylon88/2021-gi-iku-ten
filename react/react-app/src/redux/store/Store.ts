81291dc56fe02155e76ab3e50dd4b028baeedc43
GitHayato <gabayt0_0@icloud.com>
2021-08-11 16:01:02 +0900

    storeの定義

--- react/react-app/src/redux/store/Store.ts
+++ react/react-app/src/redux/store/Store.ts
@@ -8,12 +8,14 @@ import thunk from "redux-thunk"
 import { History } from "history"
 
 import { UsersReducer } from "../users/Reducers"
+import { SearchPaperReducer } from "../search/Reducers"
 
 export default function createStore(history: History) {
   return reduxCreateStore(
     combineReducers({
       router: connectRouter(history),
-      users: UsersReducer
+      users: UsersReducer,
+      search: SearchPaperReducer
     }),
     applyMiddleware(
       routerMiddleware(history),
