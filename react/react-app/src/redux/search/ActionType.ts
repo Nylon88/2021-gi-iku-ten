da06822906bc40d75870c0c3f9ffaf08e2fce8dc
GitHayato <gabayt0_0@icloud.com>
2021-08-14 18:08:48 +0900

    型定義

--- react/react-app/src/redux/search/ActionType.ts
+++ react/react-app/src/redux/search/ActionType.ts
@@ -1,9 +1,31 @@
+import { LocationState } from "history"
+
 export type searchPaper = {
   word: string;
-  result: Array<string>
+  result: Array<searchResult>
 }
 
 export type Action = {
   type: string;
   payload: searchPaper;
+}
+
+export type searchResult = {
+  abstract: string;
+  citations: string;
+  publisher: string;
+  rank: number;
+  title: string;
+  url: string;
+  writer: string;
+}
+
+export type Selector = {
+  router: LocationState ;
+  search: searchPaper;
+}
+
+export type Message = {
+  title: string,
+  status: "info" | "warning" | "success" | "error",
 }
