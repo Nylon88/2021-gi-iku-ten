edd7dacc00a471a7020e28cf4efbca864cd61a3c
GitHayato <gabayt0_0@icloud.com>
2021-08-20 21:51:50 +0900

    404ページのルーティング

--- react/react-app/src/components/router/Router.tsx
+++ react/react-app/src/components/router/Router.tsx
@@ -1,5 +1,6 @@
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router";
+import { Page404 } from "../pages/Page404";
 import { searchPaper } from "../pages/searchPaper";
 import { UserLogin } from "../pages/UserLogin";
 import { UserRegistration } from "../pages/UserRegistration";
@@ -10,6 +11,7 @@ export const Router: VFC = memo(() => {
       <Route exact path="/" component={searchPaper} />
       <Route exact path="/sign_up" component={UserRegistration} />
       <Route exact path="/sign_in" component={UserLogin} />
+      <Route path="/*" component={Page404} />
     </Switch>
   )
 })
