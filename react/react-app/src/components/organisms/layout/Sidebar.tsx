2633d72b9610614ecba82cd0463691a8b52336ed
GitHayato <gabayt0_0@icloud.com>
2021-08-25 22:04:26 +0900

    サイドバー下部の設定

--- react/react-app/src/components/organisms/layout/Sidebar.tsx
+++ react/react-app/src/components/organisms/layout/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { memo, VFC } from "react";
-import { Box, Button, Flex, Image, Link, Spacer, Text } from "@chakra-ui/react";
+import { Box, Divider, Image, Link, Text } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { push } from "connected-react-router";
 
@@ -19,7 +19,7 @@ export const Sidebar:VFC = memo(() => {
 
   return (
     <Box w="20%" h="100vh" bg="gray.100">
-      <Link onClick={() => dispatch(push("/"))}>
+      <Link onClick={() => dispatch(push("/"))} position="fixed">
         <Image
           src={`${process.env.PUBLIC_URL}/PaperPicks.png`}
           alt="PaperPicks Logo"
@@ -29,26 +29,68 @@ export const Sidebar:VFC = memo(() => {
           _hover={{cursor: "pointer"}}
         />
       </Link>
-      <Spacer />
       {
         LoginState ? (
           <>
-            <Text>
-              プロフィール
-            </Text>
-            <Text onClick={() => dispatch(signOut(showMessage))}>
-              ログアウト
-            </Text>
+            <Box ml="20" position="fixed" bottom="20">
+              <Text
+                fontSize="lg"
+                fontWeight="bold"
+                style={{cursor: "pointer"}}
+                _hover={{color: "#777"}}
+              >
+                プロフィール
+              </Text>
+              <Divider
+                w="200%"
+                mb="3"
+                borderColor="#333"
+              />
+              <Text
+                fontSize="lg"
+                fontWeight="bold"
+                style={{cursor: "pointer"}}
+                _hover={{color: "#777"}}
+                onClick={() => dispatch(signOut(showMessage))}>
+                ログアウト
+              </Text>
+              <Divider
+                w="200%"
+                mb="3"
+                borderColor="#333"
+              />
+            </Box>
           </>
         ) : (
-          <>
-            <Text onClick={() => dispatch(push("/sign_up"))} textDecoration="underline">
+          <Box ml="20" position="fixed" bottom="20">
+            <Text
+              fontSize="lg"
+              fontWeight="bold"
+              style={{cursor: "pointer"}}
+              _hover={{color: "#777"}}
+              onClick={() => dispatch(push("/sign_up"))}
+            >
               新規登録
             </Text>
-            <Text onClick={() => dispatch(push("/sign_in"))}>
+            <Divider
+              w="200%"
+              mb="3"
+              borderColor="#333"
+            />
+            <Text
+              fontSize="lg"
+              fontWeight="bold"
+              style={{cursor: "pointer"}}
+              _hover={{color: "#777"}}
+              onClick={() => dispatch(push("/sign_in"))}>
               ログイン
             </Text>
-          </>
+            <Divider
+              w="200%"
+              mb="3"
+              borderColor="#333"
+            />
+          </Box>
         )
       }
     </Box>
