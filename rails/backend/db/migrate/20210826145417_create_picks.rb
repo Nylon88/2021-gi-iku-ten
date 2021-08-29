d83c0b5225035cc697f08b692969d6c10a137c85
GitHayato <gabayt0_0@icloud.com>
2021-08-29 14:21:05 +0900

    単数形に変更＆countカラムを削除

--- rails/backend/db/migrate/20210826145417_create_picks.rb
+++ rails/backend/db/migrate/20210826145417_create_picks.rb
@@ -1,9 +1,8 @@
 class CreatePicks < ActiveRecord::Migration[6.0]
   def change
     create_table :picks do |t|
-      t.references :papers_id
-      t.references :users_id
-      t.integer :count
+      t.references :paper_id, null: false
+      t.references :user_id, null: false
 
       t.timestamps
     end
