c4c36fac841d0ae1de4a1b1d90ee4284b4b766a0
GitHayato <gabayt0_0@icloud.com>
2021-08-29 16:32:16 +0900

    カラム名の変更

--- rails/backend/db/migrate/20210826145417_create_picks.rb
+++ rails/backend/db/migrate/20210826145417_create_picks.rb
@@ -1,8 +1,8 @@
 class CreatePicks < ActiveRecord::Migration[6.0]
   def change
     create_table :picks do |t|
-      t.references :paper_id, null: false
-      t.references :user_id, null: false
+      t.references :paper, null: false
+      t.references :user, null: false
 
       t.timestamps
     end
