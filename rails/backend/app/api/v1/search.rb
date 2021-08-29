73fc1d7b9cc296f5fd8df34bb7c7b74f7f1f13c6
GitHayato <gabayt0_0@icloud.com>
2021-08-29 17:27:22 +0900

    検索結果からPick数を検索処理

--- rails/backend/app/api/v1/search.rb
+++ rails/backend/app/api/v1/search.rb
@@ -15,6 +15,13 @@ module V1
                           -k #{params[:word]} -n #{num} -y #{params[:period]}`
         # パース
         parse_value = JSON.parse(return_value)
+        # Pick数の検索
+        parse_value.each do |value|
+          paper = Paper.find_by(url: value[:url])
+          pick = paper.present? ? Pick.where(paper_id: paper.id).count : 0
+          value[:pick] = pick
+        end
+
         # フロント側に返却
         present parse_value
       end
