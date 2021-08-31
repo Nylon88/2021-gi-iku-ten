3fe1f6dd817e8134179956d7d13153afd826037d
GitHayato <gabayt0_0@icloud.com>
2021-08-31 21:41:32 +0900

    ユーザーの状態によって表示する内容を変更

--- react/react-app/src/components/pages/UserShow.tsx
+++ react/react-app/src/components/pages/UserShow.tsx
@@ -1,10 +1,28 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import auth from "../../firebase";
+import { Selector } from "../../redux/users/ActionType";
+import { getUserState } from "../../redux/users/selectors";
 
 export const UserShow: VFC = memo(() => {
+  const selector = useSelector((state: Selector) => state);
+  const userStatus = getUserState(selector);
+  const currentUserId = auth.currentUser?.uid;
+  const location = useLocation();
+
   return (
     <Flex w="80%" align="center" justify="center">
-      <Text>UserShow</Text>
+      {userStatus && location.pathname === `/users/${currentUserId}` && (
+        <Text>UserShow</Text>
+      )}
+      {userStatus && location.pathname !== `/users/${currentUserId}` && (
+        <Text fontWeight="bold">自身のプロフィールページしか閲覧できません</Text>
+      )}
+      {userStatus || (
+        <Text fontWeight="bold">ログインしてください</Text>
+      ) }
     </Flex>
   )
 })
