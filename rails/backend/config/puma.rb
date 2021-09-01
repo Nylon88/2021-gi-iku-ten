3d42dca3991da4fa00706ea0d8a51c02d5319f1b
Nylon88 <rezero0502@icloud.com>
2021-09-01 16:02:24 +0900

    ソケットファイルのみでのListen

--- rails/backend/config/puma.rb
+++ rails/backend/config/puma.rb
@@ -1,9 +1,12 @@
+# サーバ起動は rails s
+# 自動で、puma.rbを読み込みpuma.sockにバインドする。
+
 # デフォルトの設定
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 # bind        ENV.fetch("IP") { "0.0.0.0" }
-port        ENV.fetch("PORT") { 80 }
+# port        ENV.fetch("PORT") { 80 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
