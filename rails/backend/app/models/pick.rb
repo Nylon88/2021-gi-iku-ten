703a628d512325b0d29de4b168f3bf8230cbd1b8
GitHayato <gabayt0_0@icloud.com>
2021-09-04 15:02:57 +0900

    typo

--- rails/backend/app/models/pick.rb
+++ rails/backend/app/models/pick.rb
@@ -1,5 +1,5 @@
 class Pick < ApplicationRecord
-    validates :paper_id, uniquness: { scope: :user_id }
+    validates :paper_id, uniqueness: { scope: :user_id }
 
     belongs_to :user
     belongs_to :paper
