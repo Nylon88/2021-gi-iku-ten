0113489521e9a3dbb48e06c56b0bb4361ca33e0e
GitHayato <gabayt0_0@icloud.com>
2021-09-05 14:37:18 +0900

    titleタグを動的に変更

--- react/react-app/src/components/router/Router.tsx
+++ react/react-app/src/components/router/Router.tsx
@@ -1,19 +1,24 @@
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router";
+
 import { Page404 } from "../pages/Page404";
 import { searchPaper } from "../pages/searchPaper";
 import { UserLogin } from "../pages/UserLogin";
 import { UserRegistration } from "../pages/UserRegistration";
 import { UserShow } from "../pages/UserShow";
+import { Title } from "../templates/Title";
 
 export const Router: VFC = memo(() => {
   return (
-    <Switch>
-      <Route exact path="/" component={searchPaper} />
-      <Route exact path="/sign_up" component={UserRegistration} />
-      <Route exact path="/sign_in" component={UserLogin} />
-      <Route path="/users/:id" component={UserShow} />
-      <Route path="/*" component={Page404} />
-    </Switch>
+    <>
+      <Title />
+      <Switch>
+        <Route exact path="/" component={searchPaper} />
+        <Route exact path="/sign_up" component={UserRegistration} />
+        <Route exact path="/sign_in" component={UserLogin} />
+        <Route path="/users/:id" component={UserShow} />
+        <Route path="/*" component={Page404} />
+      </Switch>
+    </>
   )
 })
