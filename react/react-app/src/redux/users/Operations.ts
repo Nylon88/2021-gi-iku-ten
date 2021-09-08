121e5548cf98de4d17cb82f70dd6bfa8fb7f838d
GitHayato <gabayt0_0@icloud.com>
2021-09-08 15:07:09 +0900

    usersファイルのリファクタリング

--- react/react-app/src/redux/users/Operations.ts
+++ react/react-app/src/redux/users/Operations.ts
@@ -17,38 +17,38 @@ export const signUp = (signUpData: SignInAndUp) => {
     if (!isSignedIn) {
       // firebaseにユーザーを作成する
       await auth.createUserWithEmailAndPassword(email, password)
-        .then(async (result) => {
-          const uid = result.user?.uid
-          await axios.post(`${API_ENDPOINT}/users`, {uid})
-            .then(async () => {
-              await result.user?.updateProfile({displayName: username})
-                .then(() => {
-                  // ログインユーザー情報を取得
-                  result.user?.providerData.forEach((profile) => {
-                    // storeにユーザー情報を保存
-                    dispatch(signUpAction({
-                      id: profile?.uid,
-                      username: profile?.displayName,
-                      email: profile?.email
-                    }))
-                  })
-                  // ルートパスに移動
-                  dispatch(push("/"))
-                  // メッセージの表示
-                  showMessage({title: "正常に登録できました。", status: "success"});
-                }).catch(() => {
-                  showMessage({title: "不明なエラーです。もう一度お試しください。", status: "error"});
-                }
-              )
-            }
-          ).catch(async () => {
-            await auth.currentUser?.delete();
-            showMessage({title: "ユーザーの保存に失敗しました。もう一度お試しください。", status: "error"})
+      .then(async (result) => {
+        const uid = result.user?.uid
+        await axios.post(`${API_ENDPOINT}/users`, {uid})
+        .then(async () => {
+          await result.user?.updateProfile({displayName: username})
+          .then(() => {
+            // ログインユーザー情報を取得
+            result.user?.providerData.forEach((profile) => {
+              // storeにユーザー情報を保存
+              dispatch(signUpAction({
+                id: profile?.uid,
+                username: profile?.displayName,
+                email: profile?.email
+              }))
+            })
+            // ルートパスに移動
+            dispatch(push("/"))
+            // メッセージの表示
+            showMessage({title: "正常に登録できました。", status: "success"});
           })
-        }
-        ).catch((error) => {
-          // エラーメッセージの表示
-          if (error.code === "auth/email-already-in-use") {
+          .catch(() => {
+            showMessage({title: "不明なエラーです。もう一度お試しください。", status: "error"});
+          })
+        })
+        .catch(async () => {
+          await auth.currentUser?.delete();
+          showMessage({title: "ユーザーの保存に失敗しました。もう一度お試しください。", status: "error"})
+        })
+      })
+      .catch((error) => {
+        // エラーメッセージの表示
+        if (error.code === "auth/email-already-in-use") {
           showMessage({title: "このメールアドレスは既に使われています。", status: "error"});
         } else if (error.code === "auth/weak-password") {
           showMessage({title: "パスワードは6文字以上にしてください。", status: "error"});
@@ -69,31 +69,31 @@ export const signIn = (signInData: Omit<SignInAndUp, "username">) => {
     if (!isSignedIn) {
       // firebaseでログイン
       await auth.signInWithEmailAndPassword(email, password)
-        .then((result) => {
-          // ログインユーザー情報を取得
-          result.user?.providerData.forEach((profile) => {
-          // storeにユーザー情報を保存
-            dispatch(signInAction({
-              id: profile?.uid,
-              username: profile?.displayName,
-              email
-            }))
-          });
-          // ルートパスに移動
-          dispatch(push("/"))
-          // メッセージの表示
-          showMessage({title: "正常にログインできました。", status: "success"});
-        }).catch((error) => {
-          // エラーメッセージの表示
-          if (error.code === "auth/user-not-found") {
-            showMessage({title: "ユーザーが見つかりません。", status: "error"})
-          } else if (error.code === "auth/wrong-password") {
-            showMessage({title: "パスワードが間違っています。", status: "error"})
-          } else {
-            showMessage({title: "不明なエラーです。もう一度お試しください。", status: "error"})
-          }
+      .then((result) => {
+        // ログインユーザー情報を取得
+        result.user?.providerData.forEach((profile) => {
+        // storeにユーザー情報を保存
+          dispatch(signInAction({
+            id: profile?.uid,
+            username: profile?.displayName,
+            email
+          }))
+        });
+        // ルートパスに移動
+        dispatch(push("/"))
+        // メッセージの表示
+        showMessage({title: "正常にログインできました。", status: "success"});
+      })
+      .catch((error) => {
+        // エラーメッセージの表示
+        if (error.code === "auth/user-not-found") {
+          showMessage({title: "ユーザーが見つかりません。", status: "error"})
+        } else if (error.code === "auth/wrong-password") {
+          showMessage({title: "パスワードが間違っています。", status: "error"})
+        } else {
+          showMessage({title: "不明なエラーです。もう一度お試しください。", status: "error"})
         }
-      )
+      })
     }
   }
 }
@@ -114,7 +114,8 @@ export const signOut = (signOutData: Pick<SignInAndUp, "showMessage">) => {
         dispatch(push("/sign_in"));
         // メッセージの表示
         showMessage({title: "正常にログアウトされました。", status: "success"});
-      }).catch(() => {
+      })
+      .catch(() => {
         // エラーメッセージの表示
         showMessage({title: "不明なエラーです。もう一度お試しください。", status: "error"});
       })
