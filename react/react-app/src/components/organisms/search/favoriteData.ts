97229045aa454b6eae0082520f844181a32b7b0f
GitHayato <gabayt0_0@icloud.com>
2021-09-15 23:04:40 +0900

    型を別ファイル

---
+++ react/react-app/src/components/organisms/search/favoriteData.ts
@@ -0,0 +1,12 @@
+export type favoriteData = {
+  id: number
+  url: string
+  created_at: string
+  updated_at: string
+  title: string
+  abstract: string
+  writer: string
+  year: string
+  publisher: string
+  citations: string
+}
