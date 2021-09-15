fb2c71caec8097033ec1b445c647e6bcf4855631
GitHayato <gabayt0_0@icloud.com>
2021-09-15 23:05:01 +0900

    コンポーネントの切り出し

--- react/react-app/src/components/organisms/search/defautView.tsx
+++ react/react-app/src/components/organisms/search/defautView.tsx
@@ -1,33 +1,17 @@
 import {
   Box,
-  Flex,
-  Link,
-  LinkBox,
   TabList,
   Tab,
   TabPanel,
   TabPanels,
   Tabs,
-  Text,
-
 } from "@chakra-ui/react";
 import axios from "axios";
 import { memo, useEffect, useState, VFC } from "react";
-import { FaRegBookmark } from "react-icons/fa";
-import { API_ENDPOINT } from "../../../template/apiEndpoint";
 
-export type favoriteData = {
-  id: number
-  url: string
-  created_at: string
-  updated_at: string
-  title: string
-  abstract: string
-  writer: string
-  year: string
-  publisher: string
-  citations: string
-}
+import { API_ENDPOINT } from "../../../template/apiEndpoint";
+import { favoriteData } from "./favoriteData";
+import { ResultData } from "./resultData";
 
 export const DefaultView: VFC = memo(() => {
   const [favoriteData, setFavoriteData] = useState<favoriteData[]>([]);
@@ -55,86 +39,20 @@ export const DefaultView: VFC = memo(() => {
         <TabPanels>
           <TabPanel>
             {favoriteData.map((data, i) => (
-              <Box key={i} px="2" py="3" style={(i % 2 === 0) ? undefined : {backgroundColor: "#FAFAFA"}}>
-                <Link
-                  fontSize="lg"
-                  fontWeight="bold"
-                  color="#0055AA"
-                  href={data.url}
-                  isExternal
-                >
-                  {data.title}
-                </Link>
-                <Text fontSize="sm" mt="1" mw="100%">
-                  {data.abstract}
-                </Text>
-                <Text fontSize="xs" color="#406B15">
-                  {data.writer}・{data.year}・{data.publisher}
-                </Text>
-                <Text fontSize="xs" mt="1">
-                  <Flex align="center">
-                    <LinkBox>
-                      <Flex align="center">
-                        <FaRegBookmark />
-                        <Text>
-                          Pick数: {pickData[i]}
-                        </Text>
-                      </Flex>
-                    </LinkBox>
-                    <Text mx={2}>引用数: {data.citations}</Text>
-                    <Link
-                      href="//twitter.com/share"
-                      className="twitter-share-button"
-                      data-text={data.title}
-                      data-hashtags="PaperPicks"
-                      data-url={data.url}
-                      data-lang="ja"
-                    >ツイート</Link>
-                  </Flex>
-                </Text>
-              </Box>
+              <ResultData
+                data={data}
+                i={i}
+                pickData={pickData}
+              />
             ))}
           </TabPanel>
           <TabPanel>
             {recentData.map((data, i) => (
-              <Box key={i} px="2" py="3" style={(i % 2 === 0) ? undefined : {backgroundColor: "#FAFAFA"}}>
-                <Link
-                  fontSize="lg"
-                  fontWeight="bold"
-                  color="#0055AA"
-                  href={data.url}
-                  isExternal
-                >
-                  {data.title}
-                </Link>
-                <Text fontSize="sm" mt="1" mw="100%">
-                  {data.abstract}
-                </Text>
-                <Text fontSize="xs" color="#406B15">
-                  {data.writer}・{data.year}・{data.publisher}
-                </Text>
-                <Text fontSize="xs" mt="1">
-                  <Flex align="center">
-                    <LinkBox>
-                      <Flex align="center">
-                        <FaRegBookmark />
-                        <Text>
-                          Pick数: {pickData[i]}
-                        </Text>
-                      </Flex>
-                    </LinkBox>
-                    <Text mx={2}>引用数: {data.citations}</Text>
-                    <Link
-                      href="//twitter.com/share"
-                      className="twitter-share-button"
-                      data-text={data.title}
-                      data-hashtags="PaperPicks"
-                      data-url={data.url}
-                      data-lang="ja"
-                    >ツイート</Link>
-                  </Flex>
-                </Text>
-              </Box>
+              <ResultData
+                data={data}
+                i={i}
+                pickData={pickData}
+              />
             ))}
           </TabPanel>
         </TabPanels>
---
+++ react/react-app/src/components/organisms/search/resultData.tsx
@@ -0,0 +1,61 @@
+import { memo, VFC } from "react";
+import {
+  Box,
+  Flex,
+  Link,
+  LinkBox,
+  Text,
+} from "@chakra-ui/react";
+import { FaRegBookmark } from "react-icons/fa";
+import { favoriteData } from "./favoriteData";
+
+type Props = {
+  data: favoriteData
+  i: number
+  pickData: Number[]
+}
+
+export const ResultData: VFC<Props> = memo((props) => {
+  const { data, i, pickData } = props;
+
+  return (
+    <Box key={i} px="2" py="3" style={(i % 2 === 0) ? undefined : {backgroundColor: "#FAFAFA"}}>
+      <Link
+        fontSize="lg"
+        fontWeight="bold"
+        color="#0055AA"
+        href={data.url}
+        isExternal
+      >
+        {data.title}
+      </Link>
+      <Text fontSize="sm" mt="1" mw="100%">
+        {data.abstract}
+      </Text>
+      <Text fontSize="xs" color="#406B15">
+        {data.writer}・{data.year}・{data.publisher}
+      </Text>
+      <Text fontSize="xs" mt="1">
+        <Flex align="center">
+          <LinkBox>
+            <Flex align="center">
+              <FaRegBookmark />
+              <Text>
+                Pick数: {pickData[i]}
+              </Text>
+            </Flex>
+          </LinkBox>
+          <Text mx={2}>引用数: {data.citations}</Text>
+          <Link
+            href="//twitter.com/share"
+            className="twitter-share-button"
+            data-text={data.title}
+            data-hashtags="PaperPicks"
+            data-url={data.url}
+            data-lang="ja"
+          >ツイート</Link>
+        </Flex>
+      </Text>
+    </Box>
+  )
+})
